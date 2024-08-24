95fa6dcaa65c6941a4bf0e5a679106608748ecfb
Caden <63772723+cadencjk@users.noreply.github.com>
2024-08-24 21:46:53 +0800

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,177 @@
+const logotext = "</> Caden";
+const meta = {
+    title: "Caden's Website",
+    description: "I’m Caden _ Final year student @ National University of Singapore",
+};
+
+const introdata = {
+    title: "Hi, I’m Caden.",
+    animated: {
+        first: "I love coding",
+        second: "I develop software",
+        third: "I love photography",
+    },
+    description: "Final year student @ National University of Singapore (NUS)",};
+
+const dataabout = {
+    title: "Bio",
+    aboutme: "I am currently a final-year Computer Science undergraduate at the National University of Singapore. Throughout my academic journey, I have had the privilege of broadening my horizons by studying in Singapore, Ireland and China. These experiences have not only enriched my education but also provided me with a diverse and global perspective. My passion lies in leveraging technology to solve real-world problems, and I am eager to apply my skills and knowledge in innovative ways.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "C#",
+        value: 65,
+    },
+    {
+        name: "SQL",
+        value: 90,
+    },
+    {
+        name: "Photoshop",
+        value: 85,
+    },
+    {
+        name: "After Effects",
+        value: 75,
+    },
+];
+
+const services = [{
+        title: "Website Development",
+    },
+    {
+        title: "Mobile Apps",
+    },
+    {
+        title: "Software Engineering",
+    },
+    {
+        title: "Video Editing",
+    },
+    {
+        title: "Photography & Videography",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_e9r86mj",
+    YOUR_TEMPLATE_ID: "template_f8bd1lu",
+    YOUR_USER_ID: "AIo4sNXsdkx0xKBM9",
+};
+
+const socialprofils = {
+    github: "https://github.com/cadencjk",
+    linkedin: "https://www.linkedin.com/in/cadencjk",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
